a578e0cab0539dcf09ce46825eb1e6d3d988e2d1
Anil Patel <apatel.nz@gmail.com>
2024-08-10 16:03:52 +1200

    Completed Course Content Section 9: 114. Products Context

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -15,7 +15,7 @@ export const UserContext = createContext({
     setCurrentUser: () => null,
 });
 
-export const UserProvider = ({ children}) => {
+export const UserProvider = ({ children }) => {
     const [currentUser, setCurrentUser] = useState(null);
     const value =  { currentUser, setCurrentUser };
 
